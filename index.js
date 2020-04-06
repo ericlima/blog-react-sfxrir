69928dd8f29b57436dea3a65123e4c7a69e74e23
ericlima <eric.george.lima@gmail.com>
2020-04-06 08:44:03 +0100

    3 files modified

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
 import { render } from 'react-dom';
+import thunk from 'redux-thunk'
+
 import './style.css';
 
 import App from './src/components/app';
 import reducers from './src/reducers/';
 
+const store = createStore(reducers, applyMiddleware(thunk))
+
 render(
-  <Provider store={createStore(reducers)}>
+  <Provider store={store}>
     <App />
   </Provider>,
   document.getElementById('root')
--- src/actions/index.js
+++ src/actions/index.js
@@ -1,7 +1,11 @@
 import jsonPlaceholder from '../api/jsonplaceholder'
 
 export const fetchPosts = () => {
+
+  const promise = jsonPlaceholder.get("/posts");
+
   return {
-    type: 'FETCH_POSTS'
+    type: 'FETCH_POSTS',
+    payload: promise
   }
 }
