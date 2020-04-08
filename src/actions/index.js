a6cb5eda5aa11c4c7e184ee22031053dbe9f0a5a
ericlima <eric.george.lima@gmail.com>
2020-04-08 02:21:47 +0100

    5 files modified

--- src/actions/index.js
+++ src/actions/index.js
@@ -4,7 +4,7 @@ export const fetchPosts = () => async dispatch => {
   const response = await jsonPlaceholder.get("/posts");
   dispatch ({
     type: 'FETCH_POSTS',
-    payload: promise
+    payload: response.data
   })
 }
 
@@ -12,6 +12,6 @@ export const fetchUser = (userId) => async dispatch => {
   const response = await jsonPlaceholder.get(`/users/${userId}`);
   dispatch ({
     type: 'FETCH_USER',
-    payload: promise
+    payload: response.data
   })
 }
